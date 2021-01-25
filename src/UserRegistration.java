f44e4d0b19af2d6b1bcf3da7f440597521adb485
ARAVIND-KUMAR-POTHULA <pothulaaravind241998@gmail.com>
2021-01-25 09:57:48 +0530

    ADDED: functionality of valiation of first name and last name

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -2,14 +2,30 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("ENTER FIRST NAME");
-        String first_Name = scanner.next();
-        if(Pattern.matches("[A-Z]{1}[a-z]{2,}",first_Name)) {
-            System.out.println("VALID FIRST NAME");
+
+    public String nameValidation(String name) {
+        if(Pattern.matches("[A-Z]{1}[a-z]{2,}",name)) {
+            return "VALID";
         } else {
-            System.out.println("INVALID FIRST NAME");
+            return "INVALID";
         }
     }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String validation;
+
+        UserRegistration userRegistration = new UserRegistration();
+        System.out.println("ENTER FIRST NAME");
+        String first_Name = scanner.next();
+
+        validation = userRegistration.nameValidation(first_Name);
+            System.out.println(validation + " FIRST NAME");
+        
+        System.out.println("ENTER LAST NAME");
+        String last_Name = scanner.next();
+
+        validation = userRegistration.nameValidation(last_Name);
+        System.out.println(validation + " LAST NAME");
+        }
+
 }
